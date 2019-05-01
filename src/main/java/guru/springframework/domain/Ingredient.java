57727abfa4b48ad488e82639d5d958ca4768ad56
Giletka <mikro.is.god.@gmail.com>
2019-05-01 14:30:34 +0300

    refactoring model to mongodb

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -7,6 +7,7 @@ import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 /**
  * Created by jt on 6/13/17.
@@ -17,7 +18,7 @@ import java.math.BigDecimal;
 public class Ingredient {
 
     @Id
-    private String id;
+    private String id = UUID.randomUUID().toString();;
     private String description;
     private BigDecimal amount;
 
--- src/test/java/guru/springframework/services/RecipeServiceIT.java
+++ src/test/java/guru/springframework/services/RecipeServiceIT.java
@@ -9,6 +9,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,9 +20,8 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by jt on 6/21/17.
  */
-@Ignore
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataMongoTest
 public class RecipeServiceIT {
 
     public static final String NEW_DESCRIPTION = "New Description";
@@ -38,7 +38,6 @@ public class RecipeServiceIT {
     @Autowired
     RecipeToRecipeCommand recipeToRecipeCommand;
 
-    @Transactional
     @Test
     public void testSaveOfDescription() throws Exception {
         //given
